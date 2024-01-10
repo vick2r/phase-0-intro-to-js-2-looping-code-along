f3ed2c6f936bfb0965872568e8cf4a2a2c8b5702
Victor Njoroge <victorknjoroge@gmail.com>
2024-01-10 21:29:09 +0300

    COmpleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards (names) {
+    for (let i=0; i<names.length; i++){
+     console.log (`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return 
+}
+
+function countDown(num) {
+    while (num != -1){
+        console.log (num)
+        num--
+    }
+}
+
+console.log (writeCards(names))
